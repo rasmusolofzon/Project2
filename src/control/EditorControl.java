3253d323ce5390d528c996d5b724728e802ab040
Hagfjall <hagfjall@gmail.com>
2013-10-07 08:32:58 +0200

    Control skapad

---
+++ src/control/EditorControl.java
@@ -0,0 +1,21 @@
+package control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+import model.CellMatrix;
+
+public class EditorControl implements ActionListener {
+
+	private CellMatrix matrix;
+	public EditorControl(CellMatrix matrix, JTextField view ){
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
