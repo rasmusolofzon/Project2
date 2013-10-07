e03cea2f9a824bdf2b128f9226ce341a7e449685
Hagfjall <hagfjall@gmail.com>
2013-10-07 18:05:20 +0200

    färdigt alla klasser (inkl johans)

---
+++ src/gui/CurrentSlot.java
@@ -0,0 +1,36 @@
+package gui;
+
+import java.awt.Color;
+import java.util.Observable;
+
+import model.CellMatrix;
+
+public class CurrentSlot extends Observable {
+	private String text;
+	private CellMatrix matrix;
+	private SlotLabel currentSlot;
+
+	public CurrentSlot(CellMatrix matrix) {
+		this.matrix = matrix;
+	}
+
+	public String getCurrentSlot() {
+		return currentSlot.getAddress();
+	}
+
+	public void setCurrentSlot(SlotLabel currentSlot) {
+		this.currentSlot.setBackground(Color.WHITE);
+		currentSlot.setBackground(Color.YELLOW);
+		this.currentSlot = currentSlot;
+		setChanged();
+		notifyObservers();
+	}
+	public void initiate(SlotLabel currentSlot){
+		this.currentSlot = currentSlot;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+}
