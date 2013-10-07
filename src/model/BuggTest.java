d601150680fc925155b962eba6bee8a124c75c14
Johan Svensson <tojohansvensson@gmail.com>
2013-10-07 17:56:56 +0200

    Måndag 17.56. Model FUNGERAR!

--- src/model/BuggTest.java
+++ src/model/BuggTest.java
@@ -5,12 +5,13 @@ public class BuggTest {
 	public static void main(String[] args) {
 		CellMatrix matrix = new CellMatrix();
 
-		matrix.set("A1", "2");
-		matrix.set("A2", "1");
-		matrix.set("A4", "1");
-		matrix.set("A4", "A4");
+		matrix.set("A1", "1");
+		matrix.set("A2", "2");
+		matrix.set("A3", "A1+a2");
+		// matrix.set("A4", "1");
+		// matrix.set("A4", "A4");
 
-		double result = matrix.value("A4");
+		double result = matrix.value("A3");
 		System.out.println(result);
 
 	}
